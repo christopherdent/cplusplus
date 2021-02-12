b3545501ae7e881c10f7bd2afbad85adeb295344
Christopher Dent <christopherdent01@gmail.com>
2021-02-11 21:40:09 -0500

    Second commit

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,8 +1,32 @@
 #include<iostream>
+#include <chrono>
+#include <thread>
 using namespace std;
 
 int main()
 {
-  cout<<"Hello World!";
+  using namespace std::this_thread; // sleep_for, sleep_until
+  using namespace std::chrono; // nanoseconds, system_clock, seconds
+
+string name;
+
+  cout<<"Hello World!\n";
+
+  cout<<"What's your name?\n";
+
+  cin>>name;
+
+  cout<<"It is nice to meet you " << name << ". I am C++, remember me?";
+  cout<<"\n";
+     sleep_for(nanoseconds(10));
+     sleep_until(system_clock::now() + seconds(1));
+
+
+
+  cout<<"\n";
+     sleep_for(nanoseconds(10));
+     sleep_until(system_clock::now() + seconds(3));
+
+  cout<< "Of course you do.  It is so good to see you again," << name;
   return 0;
 }
